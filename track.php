509dbacc57ceb84122652331f7c22dfc834df64c
Peter Essien <68255976+PeterEssien@users.noreply.github.com>
2024-04-05 13:35:38 +0100

    Update track.php

--- track.php
+++ track.php
@@ -1,7 +1,3 @@
-<?php
-require('db.php');
-
-
 <!DOCTYPE html>
 <html>
 
@@ -353,7 +349,7 @@ require('db.php');
     <script src="js/wow.js"></script>
     <script src="js/jquery-ui.js"></script>
     <script src="js/script.js"></script>
-<?php } ?>
+
 </body>
 
 </html>
